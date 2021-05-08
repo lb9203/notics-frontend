2b58ec38e4fa3616c329335c7b0e83580cb9f4f6
lb9203 <lb9203@student.uni-lj.si>
2021-05-08 12:42:07 +0200

    Fix login page.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,9 +11,7 @@ function LoginPage() {
 	const [handleLogin, isLoading] = useLogin();
 	const [sessionToken, saveSessionToken] = useAuth().useSessionToken;
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-
+	const handleSubmit = async () => {
 		try {
 			const sessionToken = await handleLogin(email, password);
 			saveSessionToken(sessionToken);
@@ -31,26 +29,25 @@ function LoginPage() {
 	}
 
 	return (
-		<Form>
-			<Form.Group controlId="formBasicEmail">
-				<Form.Label>Email address</Form.Label>
-				<Form.Control type="email" placeholder="Enter email" />
-				<Form.Text className="text-muted">
-					We'll never share your email with anyone else.
-				</Form.Text>
-			</Form.Group>
+		<div id="loginFormDiv">
+			<Form>
+				<Form.Group controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" onChange={e => {setEmail(e.target.value);}}/>
+					<Form.Text className="text-muted">
+						We'll never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
 
-			<Form.Group controlId="formBasicPassword">
-				<Form.Label>Password</Form.Label>
-				<Form.Control type="password" placeholder="Password" />
-			</Form.Group>
-			<Form.Group controlId="formBasicCheckbox">
-				<Form.Check type="checkbox" label="Check me out" />
-			</Form.Group>
-			<Button variant="primary" type="submit">
-				Submit
-			</Button>
-		</Form>
+				<Form.Group controlId="formBasicPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" onChange={e => {setPassword(e.target.value);}}/>
+				</Form.Group>
+				<Button variant="primary" type="button" onClick={() => handleSubmit()}>
+					Submit
+				</Button>
+			</Form>
+		</div>
 	);
 }
 
