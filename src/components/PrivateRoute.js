90544ef1f61a3a68952896fc3f49b62a67f8bee4
lb9203 <lb9203@student.uni-lj.si>
2021-05-09 13:51:35 +0200

    Fix PrivateRoute.

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -13,7 +13,7 @@ function PrivateRoute({ children, ...rest }) {
 				) : (
 					<Redirect
 						to={{
-							pathname: "/login",
+							pathname: "/auth",
 							state: { from: location }
 						}}
 					/>
